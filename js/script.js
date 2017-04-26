787bef41fc3633e8d12b0dd4bec1331e1da65527
Mai <maifujii619@gmail.com>
2017-04-26 13:42:56 -0700

    adds code for slideshow on homepage

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// carousel slideshow on homepage - code from w3schools website
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slideshow");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 4000); 
+}
