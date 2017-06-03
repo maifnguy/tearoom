c35330e77c25638e57b140b77647b5925431adff
Mai <maifujii619@gmail.com>
2017-06-02 18:06:42 -0700

    adds code for slippy map

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// carousel slideshow on homepage - code from w3schools website
+// HOME -- carousel slideshow on homepage - code from w3schools website
 var slideIndex = 0;
 carousel();
 
@@ -11,5 +11,39 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 4000); 
-}
+    setTimeout(carousel, 2800); 
+};
+
+
+function initMap () {
+  var map = new google.maps.Map( $('#google-map')[0], { 
+    center: { lat: 47.6062, lng: -122.3321 },
+    zoom: 10
+  })
+
+  $('#select').on('change', function () {
+    if($(this).val() == 'seattle') {
+      var latLng = new google.maps.LatLng(47.6062, -122.3321);
+      map.setZoom (15);
+      map.panTo(latLng);
+
+      var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(47.6062, -122.3321),
+      title: 'Marker',
+      map: map,
+      draggable: true
+      });
+    } else if ($(this).val() == 'cambridge') {
+      var latLng = new google.maps.LatLng(42.3736, -71.1097);
+      map.setZoom (15);
+      map.panTo(latLng);
+
+      var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(42.3736, -71.1097),
+      title: 'Marker',
+      map: map,
+      draggable: true
+
+    })}
+  })
+};
