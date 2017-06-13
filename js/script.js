a13417e8edcfd0b941dac1ebd841d490595b3d01
Mai <maifujii619@gmail.com>
2017-06-13 16:49:21 -0700

    adds code to console.log form input

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
-// HOME -- carousel slideshow on homepage - code from w3schools website
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("slideshow");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2800); 
-};
+// CONTACT -- console.logs form values
+$('#submit').on('click', log);
+function log () {
+  console.log('Name: ' + $('#name').val() + ' Phone: ' + $('#tel').val() + ' Email: ' + $('#email').val() + ' Location: ' + $('#location').val() + ' Event: ' + $('#event').val() + ' Message: ' + $('#message').val());
+}
 
 
+// CONTACT -- slippy map 
 function initMap () {
   var map = new google.maps.Map( $('#google-map')[0], { 
     center: { lat: 47.6062, lng: -122.3321 },
-    zoom: 10
-  })
+    zoom: 15
+  });
+  var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(47.6062, -122.3321),
+      title: 'Marker',
+      map: map,
+      draggable: true
+      });
 
   $('#select').on('change', function () {
     if($(this).val() == 'seattle') {
@@ -47,3 +44,22 @@ function initMap () {
     })}
   })
 };
+
+// HOME -- carousel slideshow on homepage 
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName('slideshow');
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = 'none'; 
+    };
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = 'block'; 
+    setTimeout(carousel, 2800); 
+};
+
+
+
